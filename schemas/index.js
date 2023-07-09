41f71acd70d4d2e2e1ca7aa05908f8e92982193f
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-09 17:26:51 +0300

    конфлікт help meee

--- schemas/index.js
+++ schemas/index.js
@@ -1,15 +1,9 @@
-const {
-  createContact,
-  updateContact,
-  updateFavoriteSchema,
-} = require("./contacts");
+
 
 const { registerSchema, loginSchema, updateUserSchema } = require("./users");
 
 module.exports = {
-  createContact,
-  updateContact,
-  updateFavoriteSchema,
+
   registerSchema,
   loginSchema,
   updateUserSchema
