e811ba8a65e7697d175b4ca80a10b0e16dbbf870
Gogolbog <111005996+Gogolbog@users.noreply.github.com>
2023-07-10 00:44:17 +0300

    changed token duration

--- controllers/auth/userLogin.js
+++ controllers/auth/userLogin.js
@@ -18,14 +18,14 @@ const login = async (req, res, next) => {
   const payload = {
     id: user._id,
   };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
   await User.findByIdAndUpdate(user._id, { token });
   res.status(200).json({
     message: "Successful login",
     token,
     user: {
       email: user.email,
-      name: user.name
+      name: user.name,
     },
   });
 };
