fce24ce9fd9776ab7f2713d5d4d3b87990408e18
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-10 00:24:05 +0300

    Update handleMongooseError.js

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,6 +1,7 @@
 const  handleMongooseError = (error, data, next) => {
     const {name, code} = error
-    console.log(name);
+    console.log(name); 
+    
     const status = (name === "MongoServerError" && code === 11000) ? 409 : 400
         error.status = status;
         next()
