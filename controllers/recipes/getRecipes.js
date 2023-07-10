4bdaf16a21c796227eb190b2f081e9254db41bfd
Vitaliy117979 <108054894+Vitaliy117979@users.noreply.github.com>
2023-07-10 16:02:11 +0300

    Update getRecipes.js

--- controllers/recipes/getRecipes.js
+++ controllers/recipes/getRecipes.js
@@ -1,9 +1,9 @@
-const Resipe = require("../../models/recipe")
+const Reсipe = require("../../models/recipe")
 const Category = require("../../models/category")
 
 async function getRecipesList(req, res, ) {
     console.log(req.user);
-    const list = await Resipe.find().populate("ingredient")
+    const list = await Reсipe.find().populate("ingredient")
     console.log(list);
         res.status(200).json(list);
    
