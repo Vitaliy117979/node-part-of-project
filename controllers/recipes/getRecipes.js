8699ee2e2059a4219a609444d365d396773f23a3
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-11 10:37:53 +0300

    Update getRecipes.js

--- controllers/recipes/getRecipes.js
+++ controllers/recipes/getRecipes.js
@@ -1,7 +1,8 @@
 const {Recipe} = require("../../models/recipe");
+console.log(Recipe);
 async function getRecipesList(req, res) {
   // console.log(req.user);
-  const list = await Recipe.find()
+  const list = await Recipe.find().populate("ingredients.id");
   console.log(list);
   res.status(200).json(list);
 }
