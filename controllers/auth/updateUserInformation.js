f5b32f8ec4b66844fd4d28c4725c7baeeadadafb
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-14 13:14:22 +0300

    updata

--- controllers/auth/updateUserInformation.js
+++ controllers/auth/updateUserInformation.js
@@ -1,38 +1,38 @@
 const User = require("../../models/user");
+const { uploadFile } = require("../../middleware");
+
 
 const updateUserInformation = async (req, res, next) => {
+  console.log(req.file.path);
   const { name } = req.body;
-  console.log(req.body);
   const { _id } = req.user;
-  if (req.file) {
-    if (!name) {
-      return res.status(400).json({ message: "Name field cannot be empty." });
-    }
-    const { path: filename, size } = req.file;
-    if (size > 3 * 1024 * 1024) {
-      return res.status(400).json({ message: "File size exceeds the limit." });
-    }
-
-    const updates = {
-      name,
-      avatarURL: filename,
-    };
-    await User.findByIdAndUpdate(_id, { $set: updates });
-    return res.status(201).json({
-      name: name,
-      avatarURL: updates.avatarURL,
-    });
-  }
-
   if (!name) {
     return res.status(400).json({ message: "Name field cannot be empty." });
   }
 
+  if (req.file) {
+    if (req.file.size > 3 * 1024 * 1024) {
+      return res.status(400).json({ message: "File size exceeds the limit." });
+    }
+    const { filename } = req.file;
+    const result = await uploadFile(req.file.fieldname, filename);
+
+    const updates = {
+      name,
+      avatarURL: result.secure_url,
+    };
+
+    await User.findByIdAndUpdate(_id, { $set: updates });
+
+    return res.status(201).json({
+      name: updates.name,
+      avatarURL: updates.avatarURL,
+    });
+  }
+
   await User.findByIdAndUpdate(_id, { name });
-  res.status(201).json({
-    name,
-  });
+
+  res.status(201).json({ name });
 };
 
 module.exports = { updateUserInformation };
-// updateUserInformation
--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,6 @@ const  authenticate = require("./authenticate")
 const  isValidId = require("./isValidId")
 const validate = require("./validation")
 const upload = require("./upload")
+const uploadFile = require("./uploadToCloudinary")
 
-
-module.exports = {authenticate, isValidId, validate, upload}
+module.exports = {authenticate, isValidId, validate, upload, uploadFile}
