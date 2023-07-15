9b639a5a5e796994f5ca6e2fb4ce00f6e88f332f
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-15 14:33:27 +0300

    updata model and schema

--- models/product.js
+++ models/product.js
@@ -1,33 +1,36 @@
+const { ObjectId } = require("mongodb");
 const { Schema, model } = require("mongoose");
 
 const productSchema = Schema(
-        {
-          name: {
-            type: String,
-            required: true,
-          },
-          desc: {
-            type: String,
-            required: true,
-          },
-          img: {
-                type: String,
-           },
-          measure: {
-            type: String,
-            required: true,
-          },
-          owner: {
-            type: Schema.Types.ObjectId,
-            ref: "user",
-            required: true,
-          },
-        },
-        { timestamps: true }
-      );
-      
-      const Product = model("product", productSchema);
-      
-      module.exports = Product
-      
-      
+  {
+    _id: {
+      type: ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    measure: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = model("product", productSchema);
+
+module.exports = Product;
