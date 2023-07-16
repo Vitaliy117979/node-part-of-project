1176e9bdb8dbcbfba3623ee3da1d53b4c9e76285
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-16 17:18:02 +0300

    wr

--- schemas/shoppingList.js
+++ schemas/shoppingList.js
@@ -2,11 +2,11 @@ const Joi = require("joi");
 
 const buyIngredientSchema = Joi.object({
   _id: Joi.string().required(),
- newId: Joi.string().required(),
+  newId: Joi.string().required(),
   name: Joi.string().required(),
   desc: Joi.string().required(),
   img: Joi.string().required(),
-  measure:Joi.string().required(),
+  measure: Joi.string().required(),
 });
 
 module.exports = buyIngredientSchema;
