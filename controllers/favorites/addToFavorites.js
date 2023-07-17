9e83b916e0d96a26e6ce8dd5cb091fa8f66c7d8d
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-17 19:11:30 +0300

    rr

--- controllers/favorites/addToFavorites.js
+++ controllers/favorites/addToFavorites.js
@@ -1,27 +1,27 @@
 const User = require("../../models/user");
 const Recipe = require("../../models/recipe");
+
 const addToFavorites = async (req, res, next) => {
-  const recipeId = req.body._id
+  const recipeId = req.body._id;
   const userId = req.user._id;
   // console.log(recipeId);
-  try {
-    const user = await User.findByIdAndUpdate(userId, {
-      $addToSet: { favorites: recipeId },
-    });
-    // console.log(user);
-    if (user.favorites.includes(recipeId)) {
-      
-    return  res.status(409).json({ message: "Recipe is already added to your favorites" });
-    }
-    if (user) {
-      await Recipe.findByIdAndUpdate(recipeId, { $inc: { popularity: 1 } });
+  const user = await User.findByIdAndUpdate(userId, {
+    $addToSet: { favorites: recipeId },
+  });
+  // console.log(user);
+  if (user.favorites.includes(recipeId)) {
+    return res
+      .status(409)
+      .json({ message: "Recipe is already added to your favorites" });
+  }
+  if (user) {
+    await Recipe.findByIdAndUpdate(recipeId, { $inc: { popularity: 1 } });
 
-      res
-        .status(200)
-        .json({ message: "Recipe added to favorites successfully" });
-    }
-  } catch (error) {
-    next(error);
+    const favoritesArray = req.user.favorites;
+    const result = await Recipe.find({ _id: { $in: favoritesArray } }).populate(
+      "ingredients.id"
+    )
+    res.status(200).json(result);
   }
 };
 
