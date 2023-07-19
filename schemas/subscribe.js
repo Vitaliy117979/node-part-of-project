a676b1f244c4e633a7f39d461c45f63a44106186
Vitaliy117979 <privedenyuk2006@gmail.com>
2023-07-19 14:09:41 +0300

    add notification

--- schemas/subscribe.js
+++ schemas/subscribe.js
@@ -3,8 +3,18 @@ const Joi = require("joi");
 const subscrTypes = ["subscribe", "unsubscribe"];
 
 const updateSubscriptionSchema = Joi.object().keys({
-        subscription: Joi.string().valid(...subscrTypes).required(),
-        email: Joi.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).required(),
+  subscription: Joi.string()
+    .valid(...subscrTypes)
+    .required(),
+  email: Joi.string()
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+    .required()
+    .messages({
+      "string.base": "The email must be a string.",
+      "any.required": "The email field is required.",
+      "string.empty": "The email must not be empty",
+      "string.pattern.base": "The email must be in format test@gmail.com.",
+    }),
 });
 
 module.exports = { updateSubscriptionSchema };
--- schemas/users.js
+++ schemas/users.js
@@ -1,19 +1,55 @@
 const Joi = require("joi");
 
 const registerSchema = Joi.object().keys({
-  email: Joi.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).required(),
-  password: Joi.string().min(8).required(),
-  name: Joi.string().min(2).required(),
+  email: Joi.string()
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+    .required()
+    .empty(false)
+    .messages({
+      "string.base": "The email must be a string.",
+      "any.required": "The email field is required.",
+      "string.empty": "The email must not be empty",
+      "string.pattern.base": "The email must be in format test@gmail.com.",
+    }),
+  password: Joi.string().min(8).required().empty(false).messages({
+    "string.base": "The password must be a string.",
+    "any.required": "The password field is required.",
+    "string.empty": "The password must not be empty",
+  }),
+  name: Joi.string().min(2).required().empty(false).messages({
+    "string.base": "The name must be a string.",
+    "any.required": "The name field is required.",
+    "string.empty": "The name must not be empty",
+    "string.min": "The password must be not less 2 symbols.",
+  }),
   subscription: Joi.string(),
 });
 
 const loginSchema = Joi.object().keys({
-  email: Joi.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).required(),
-  password: Joi.string().min(8).required(),
+  email: Joi.string()
+    .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+    .empty(false)
+    .required()
+    .messages({
+      "string.base": "The email must be a string.",
+      "any.required": "The email field is required.",
+      "string.empty": "The email must not be empty",
+      "string.pattern.base": "The email must be in format test@gmail.com.",
+    }),
+  password: Joi.string().min(8).empty(false).required().messages({
+    "string.base": "The password must be a string.",
+    "any.required": "The password field is required.",
+    "string.empty": "The password must not be empty",
+    "string.min": "The password must be not less 8 symbols.",
+  }),
 });
 
 const updateUserSchema = Joi.object().keys({
-  name: Joi.string().min(2).required(),
+  name: Joi.string().min(2).empty(false).required().messages({
+    "string.base": "The name must be a string.",
+    "any.required": "The name field is required.",
+    "string.empty": "The name must not be empty",
+  }),
 });
 
 module.exports = { registerSchema, loginSchema, updateUserSchema };
